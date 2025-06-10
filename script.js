b2e29dbaae634249f79356fdc3ecc7a3ed30802a
NavNomad <gonavnomad@gmail.com>
2025-06-09 19:43:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,110 +1,88 @@
-// script.js
 angular.module("app", ["leaflet-directive"])
 .controller("GermanMapCtrl", ["$scope", "$http", function($scope, $http) {
-  $scope.showCenterCoords = false;
-  $scope.countriesGeoJson = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/93930/countries.geo.json?v=11";
-  $scope.countriesJson = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/93930/countries.json?v=11";
+  $scope.center = { lat: 20, lng: 0, zoom: 2 };
+  $scope.defaults = {
+    tileLayer: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  };
+  $scope.selectedCountry = {};
+  $scope.selectedCountryData = {};
+  $scope.highlightedLayer = null;
 
-  const continentProperties = {
-    "150": { name: "Europe", colors: ["#809bce", "#7291cc", "#6983b3", "#6d90d1", "#506ca1"] },
-    "019": { name: "America", colors: ["#81d4fa", "#4fc3f7", "#29b6f6", "#03a9f4", "#039be5"] },
-    "002": { name: "Africa", colors: ["#b8e0d4", "#a3c4ba", "#a3d4c5", "#88bdad", "#91e3ca"] },
-    "142": { name: "Asia", colors: ["#eac4d5", "#dea0bc", "#d1a3b8", "#eda6c6"] },
-    "009": { name: "Oceania", colors: ["#d6eadf", "#deffed", "#bae3cd", "#9dc4af", "#cef0de"] }
+  const continentColors = {
+    "150": ['#809bce', '#7291cc', '#6983b3', '#6d90d1', '#506ca1'], // Europe
+    "019": ['#8cd2e8', '#7ec8df', '#6fbfd7', '#62b5cf', '#56abc7'], // America (cyan tones)
+    "002": ['#b8e0d4', '#a3c4ba', '#a3d4c5', '#88bdad', '#91e3ca'], // Africa
+    "142": ['#eac4d5', '#dea0bc', '#d1a3b8', '#eda6c6', '#e6accf'], // Asia
+    "009": ['#d6eadf', '#deffed', '#bae3cd', '#9dc4af', '#cef0de']  // Oceania
   };
 
-  angular.extend($scope, {
-    center: {
-      lat: 20,
-      lng: 0,
-      zoom: 2
-    },
-    legend: {
-      colors: ["#809bce", "#81d4fa", "#b8e0d4", "#eac4d5", "#d6eadf"],
-      labels: ["Europe", "America", "Africa", "Asia", "Oceania"]
-    },
-    defaults: {
-      tileLayer: "http://{s}.tiles.wmflabs.org/osm-no-labels/{z}/{x}/{y}.png"
-    }
-  });
-
   function getColor(country) {
-    if (!country || !country["region-code"]) return "#ccc";
-    const colors = continentProperties[country["region-code"]]?.colors || ["#ccc"];
-    const index = country["alpha-3"].charCodeAt(0) % colors.length;
+    if (!country || !country["region-code"]) return "#FFF";
+    const colors = continentColors[country["region-code"]] || ['#ccc'];
+    const index = country["alpha-3"]?.charCodeAt(0) % colors.length;
     return colors[index];
   }
 
-  function style(feature) {
-    const country = $scope.countries[feature.id];
-    return {
-      fillColor: getColor(country),
-      weight: 1,
-      opacity: 1,
-      color: "white",
-      fillOpacity: 0.7
-    };
-  }
-
-  function highlightLayer(layer, color = "#000", fillColor = "#39e") {
+  function highlightFeature(layer) {
+    if ($scope.highlightedLayer) {
+      $scope.highlightedLayer.setStyle({
+        weight: 1,
+        color: "#fff",
+        fillColor: $scope.highlightedLayer.defaultFill
+      });
+    }
+    layer.defaultFill = layer.options.fillColor;
     layer.setStyle({
-      weight: 2,
-      color: color,
-      fillColor: fillColor
+      weight: 3,
+      color: "#000",
+      fillColor: "#39e"
     });
-    layer.bringToFront();
+    $scope.highlightedLayer = layer;
   }
 
-  function resetHighlight(layer, country) {
-    layer.setStyle({
-      weight: 1,
-      color: "white",
-      fillColor: getColor(country)
+  // Load country metadata
+  $http.get("https://s3-us-west-2.amazonaws.com/s.cdpn.io/93930/countries.json?v=11").then(res => {
+    const countriesMeta = {};
+    res.data.forEach(c => {
+      c["alpha-3"] = c["alpha-3"]?.replace("_", "");
+      c.flag = (c["alpha-2"] || "").toLowerCase();
+      countriesMeta[c["alpha-3"]] = c;
     });
-  }
+    $scope.countries = countriesMeta;
 
-  $http.get($scope.countriesJson).then(function(res) {
-    const data = res.data;
-    $scope.countries = {};
-    data.forEach(country => {
-      const id = country["alpha-3"].replace("_", "");
-      country.flag = country["alpha-2"]?.toLowerCase() || "";
-      $scope.countries[id] = country;
-    });
-
-    $http.get($scope.countriesGeoJson).then(function(geo) {
+    // Now load the geojson data
+    $http.get("https://s3-us-west-2.amazonaws.com/s.cdpn.io/93930/countries.geo.json?v=11").then(geo => {
       $scope.geojson = {
         data: geo.data,
-        style: style,
-        resetStyleOnMouseout: true,
-        onEachFeature: function(feature, layer) {
-          layer.on({
-            click: function(e) {
-              const country = $scope.countries[feature.id];
-              $scope.selectedCountry = feature;
-              $scope.selectedCountryData = country || {};
+        style: feature => {
+          const countryMeta = countriesMeta[feature.id];
+          return {
+            fillColor: getColor(countryMeta),
+            weight: 1,
+            color: "white",
+            fillOpacity: 0.7
+          };
+        },
+        onEachFeature: (feature, layer) => {
+          layer.on("click", () => {
+            highlightFeature(layer);
+            const code = feature.id;
+            $scope.selectedCountryData = countriesMeta[code] || {};
 
-              $http.get(`https://restcountries.com/v3.1/alpha/${feature.id}`)
-                .then(res => {
-                  const info = res.data[0];
-                  $scope.selectedCountryExtra = {
-                    language: Object.values(info.languages || {})[0] || "Unknown",
-                    currency: Object.values(info.currencies || {})[0]?.name || "Unknown",
-                    symbol: Object.values(info.currencies || {})[0]?.symbol || ""
-                  };
-                }).catch(() => {
-                  $scope.selectedCountryExtra = { language: "Unknown", currency: "Unknown", symbol: "" };
-                });
-
-              highlightLayer(layer);
-              $scope.$applyAsync();
-            },
-            mouseover: function(e) {
-              highlightLayer(layer, "#666", "#ddd");
-            },
-            mouseout: function(e) {
-              resetHighlight(layer, $scope.countries[feature.id]);
-            }
+            // Live fetch language + currency
+            $http.get(`https://restcountries.com/v3.1/alpha/${code}`).then(res => {
+              const data = res.data[0];
+              $scope.selectedCountry = {
+                name: data.name.common,
+                language: Object.values(data.languages || {})[0] || "Unknown",
+                currency: Object.values(data.currencies || {})[0]?.name || "Unknown",
+                symbol: Object.values(data.currencies || {})[0]?.symbol || ""
+              };
+            }).catch(() => {
+              $scope.selectedCountry = {
+                name: "Unknown", language: "Unknown", currency: "Unknown", symbol: ""
+              };
+            });
           });
         }
       };
